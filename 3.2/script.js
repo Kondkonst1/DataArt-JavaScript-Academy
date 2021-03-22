ea787aefd669083e70b78acae46bd99873b03708
Konst <kondkonst@gmail.com>
2021-03-22 23:39:58 +0300

    22.03.21

--- 3.2/script.js
+++ 3.2/script.js
@@ -1,76 +1,103 @@
 'use strict'
 const urlApi = 'https://api.exchangeratesapi.io/';
-let baseList = [];
-let dateList = [];
 const countDay = 7;
-const days = [];
-const data = {};
+const requestParam = {};
 const app = document.querySelector('#app');
+const wrapperTable = document.createElement('div');
+const wrapperButton = document.createElement('div');
+const wrapperBase = document.createElement('div');
 
-
-const setDate = () =>{
-    let currentDay='';
-    for (let i=0; i<countDay; i++){
-    let today = new Date();
-    today.setDate(today.getDate() - i);
-    let year = today.getFullYear();
-    let month = today.getMonth()+1;
-    let day = today.getDate();
-    if (day < 10){
-        day = '0'+day;
-    }
-    if (month<10){
-        month = '0'+month;
-    }
-    currentDay = `${year}-${month}-${day}`;
-    days.push(currentDay);
-    }
+let daysArray = Array.from(Array(7), (_, i) => {
+    let date = new Date();
+    date.setDate(date.getDate() - i);
+    return date.toISOString().slice(0, 10)
+});
+const setParameter = (day = daysArray[0], currency = 'RUB') => {
+    requestParam.date = day;
+    requestParam.currency = currency;
 }
-
 
 const getCourse = async (data) => {
+
     const url = `${urlApi}${data.date}?base=${data.currency}`;
     const dataAnswer = await fetch(url);
-    let jsonData;
     if (dataAnswer.ok) {
-        jsonData = await dataAnswer.json();
-        baseList = Object.keys(jsonData.rates);
-       
+        return await dataAnswer.json();
     }
-
-    return jsonData;
 }
 
+const drawButtons = () => {
 
-const drawButtons = () =>{
-    const wrapperButton = document.createElement('div');
     wrapperButton.classList.add('wrapper-button')
     app.appendChild(wrapperButton);
-    const buttons = days.map(item => {
-      return  `<button>${item}</button>`;
+    const buttons = daysArray.map(item => `<button>${item}</button>`);
+    wrapperButton.innerHTML = buttons.join('');
+
+    wrapperButton.addEventListener('click', (e) => {
+        updateTable(e.target.innerHTML);
+    })
+}
+
+const updateTable = (date) => {
+    const selectedItem = document.getElementById('setCurrency');
+    const valueCurrency = selectedItem.options[selectedItem.selectedIndex].value;
+    setParameter(date, valueCurrency);
+    drawTable();
+};
+
+const drawBaseChange = async () => {
+
+    const bases = document.createElement('select');
+    bases.setAttribute('id', 'setCurrency');
+    app.appendChild(wrapperBase);
+    wrapperBase.innerHTML = '<h1>Курсы валют</h1><p>Выберите базовую валюту</p>';
+    const updateData = await getCourse(requestParam);
+    const currencys = Object.keys(updateData.rates).map(item => {
+        return `<option>${item}</option>`
     });
 
-    wrapperButton.innerHTML = buttons.join("");
-console.log(wrapperButton);
+    bases.innerHTML = currencys.join('');
+    bases.value = requestParam.currency;
+    wrapperBase.appendChild(bases);
+    wrapperTable.classList.add('tableWrapper');
+    app.appendChild(wrapperTable);
+    wrapperBase.classList.add('wrapper-base');
 
-   
-
+    bases.addEventListener('change', (e) => {
+        updateTable(requestParam.date);
+    });
 }
 
+const drawTable = async () => {
 
-const drawBase = () =>{
+    wrapperTable.innerHTML = `<p>Валюта:${requestParam.currency}</p><p>  Дата:${requestParam.date} </p>`;
+    const table = document.createElement('table');
+    const tr = document.createElement('tr');
+    const th = document.createElement('th');
+    const th2 = document.createElement('th');
+    th.textContent = "Валюта";
+    th2.textContent = "Цена";
+    tr.appendChild(th);
+    tr.appendChild(th2);
+    table.appendChild(tr);
+    wrapperTable.appendChild(table);
+    const updateData = await getCourse(requestParam);
 
+    for (let rate in updateData.rates) {
+        const tr = document.createElement("tr");
+        const td = document.createElement("td");
+        const td2 = document.createElement("td");
+        td.textContent = rate;
+        td2.textContent = updateData.rates[rate];
+        tr.appendChild(td);
+        tr.appendChild(td2);
+        table.appendChild(tr);
+    }
+    app.appendChild(wrapperTable);
 }
 
-const drawRates = () =>{
-
-
-}
-
-
-
-setDate();
- data.date = days[0];
- data.currency = 'RUB';
-getCourse(data);
+setParameter();
+getCourse(requestParam);
+drawBaseChange();
 drawButtons();
+drawTable();
