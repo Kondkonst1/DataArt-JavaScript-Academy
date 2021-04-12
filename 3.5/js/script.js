f708d7dee44e6c8746c49f98d97d168ee6cff126
konstantin <kostik1992@mail.ru>
2021-04-12 12:03:01 +0300

    debug remove

--- 3.5/js/script.js
+++ 3.5/js/script.js
@@ -1,130 +1,129 @@
-'use strict'
+"use strict";
 
-class ToDoApp{
-    constructor() {
-        this.addToDoButton = document.querySelector('.todo-list__button');
-        this.messageText = document.querySelector('.todo-list__input');
-        this.todoList = [];
-        this.todoListBlock = document.querySelector('.todo-list__items');
-    }
+class ToDoApp {
+  constructor() {
+    this.addToDoButton = document.querySelector(".todo-list__button");
+    this.messageText = document.querySelector(".todo-list__input");
+    this.todoListBlock = document.querySelector(".todo-list__items");
+    this.todoList = [];
+  }
 
-    addToDoItem = () => {
-            const newToDo = {
-            todo: this.messageText.value,
-            done: false
-        };
-        this.todoList.push(newToDo);
-        this.todoList.forEach((item, i) => {
-         let oneTodoItem = ` 
-            <div class = "todo-list__deal">
-                <div id='item_${i}' class="todo-list__deal-link">
-                    <a href="${item.todo}" target="_blank" data-logged="true">${item.todo}</a>
-                </div>
-                <button class="todo-list__del" data-logged="true">Удалить</button>
-            </div>
+  addToDoItem = () => {
+    if (this.messageText.value !== "") {
+      const newToDo = {
+        todo: this.messageText.value,
+        done: false,
+      };
+      this.todoList.push(newToDo);
+      const deal = document.createElement("div");
+      deal.classList.add("todo-list__deal");
+      const oneTodoItem = ` 
+        <div class="todo-list__deal-link">
+        <a href="${newToDo.todo}" target="_blank" data-logged="true">${newToDo.todo}</a>
+        </div>
+        <button class="todo-list__del" data-logged="true">Удалить</button>
         `;
-            this.todoListBlock.innerHTML+=oneTodoItem; 
-            const delButton = document.querySelector('.todo-list__del');
-            delButton.addEventListener('click', (e)=>this.deleleItem(e));
-            }) 
-            
-    };
+      deal.insertAdjacentHTML("beforeEnd", oneTodoItem);
+      this.todoListBlock.appendChild(deal);
 
-deleleItem = (e) => {
-   e.target.parentElement.remove();
-   e.stopPropagation();
+      const delButton = deal.querySelector(".todo-list__del");
+      this.deleleItem(delButton);
+      this.messageText.value = "";
+    } else {
+      alert("Введите ссылку");
+    }
+  };
 
-}
-
-init = () => {
-    this.addToDoButton.addEventListener('click', () => {
-        this.addToDoItem();
+  deleleItem = (el) => {
+    el.addEventListener("click", (event) => {
+      el.parentElement.remove();
     });
-}
+  };
+
+  init = () => {
+    this.messageText.addEventListener("keypress", (keyPressed) => {
+      const keyEnter = 13;
+      if (keyPressed.which == keyEnter) {
+        this.addToDoItem();
+      }
+    });
+
+    this.addToDoButton.addEventListener("click", () => {
+      this.addToDoItem();
+    });
+  };
 }
 
 class ClickTracker {
-    constructor() {
-        this.buttonShowLog = document.querySelector('.click-info__button');
-        this.logContainer = document.querySelector('.click-info__wrapper');
-        this.logArray = [];
-    }
-    getTime = () => {
-        const now = new Date();
-        return now.toLocaleString();
-    }
-    init = () => {
-        this.buttonShowLog.addEventListener('click', this.showLog);
-        document.querySelectorAll('[data-logged]');
-        document.addEventListener('click', (ev) => {
-            if (ev.target.dataset.logged === 'true') {
-                this.writeLog(ev);
-            }
-        })
-    }
-    writeLog = (ev) => {
+  constructor() {
+    this.buttonShowLog = document.querySelector(".click-info__button");
+    this.logContainer = document.querySelector(".click-info__wrapper");
+    this.logArray = [];
+  }
+  getTime = () => {
+    const now = new Date();
+    return now.toLocaleString();
+  };
+  init = () => {
+    this.buttonShowLog.addEventListener("click", this.showLog);
+    document.addEventListener("click", (ev) => {
+      if (ev.target.dataset.logged === "true") {
+        this.writeLog(ev);
+      }
+    });
+  };
 
-        //в отдельный метод
-        const pathArr = [];
-        ev.path.forEach((item, index) => {
-            if (index < ev.path.length - 2) {
-                pathArr.push(
-                    `${item.localName}${
-                    item.className? "." + item.className: 
-                    item.id? "#" + item.id: " "
-                  }`
-                );
-            }
-        });
-        pathArr.reverse();
-        this.putLogItem({
-            tag: ev.target.tagName,
-            innerText: ev.target.innerText,
-            time: this.getTime(),
-            path: pathArr.join(" => ")
-        });
-    }
+  writeLog = (ev) => {
+    this.putLogItem({
+      tag: ev.target.tagName,
+      innerText: ev.target.innerText,
+      time: this.getTime(),
+      path: ev.path
+        .map((item) => (item.className ? item.className : item.nodeName))
+        .reverse()
+        .join(" => "),
+    });
+  };
 
-    putLogItem = (item) => {
-        this.logArray.push(item);
-    }
-    getLog = () => {
-        return this.logArray;
-    }
-    showLog = () => {
-        this.logContainer.innerHTML = '';
-        const allItems = this.getLog();
-        this.logContainer.innerHTML = this.renderLog(allItems);
-    }
-    renderLog = (logItems) => {
-        return ` <table>
+  putLogItem = (item) => {
+    this.logArray.push(item);
+  };
+  getLog = () => {
+    return this.logArray;
+  };
+  showLog = () => {
+    this.logContainer.innerHTML = "";
+    const allItems = this.getLog();
+    this.logContainer.innerHTML = this.renderLog(allItems);
+  };
+  renderLog = (logItems) => {
+    return ` <table>
         <tr>
-            <th>NodeItem</th>
-            <th>InnerText</th>
-            <th>TIME</th>
-            <th>PATH</tr>
+            <th>Node Item</th>
+            <th>Inner Text</th>
+            <th>Time</th>
+            <th>Path</tr>
         </tr>
-        ${   
-            logItems.map(item=>{return `
+        ${logItems
+          .map((item) => {
+            return `
                 <tr>
-                    <td>${item.tag}</td>
+                    <td>${item.tag.toLowerCase()}</td>
                     <td>${item.innerText}</td>
                     <td>${item.time}</td>
                     <td>${item.path}</td>
                 </tr>
-            `
-            }).join('')}</table>`
-        }
-    }
+            `;
+          })
+          .join("")}</table>`;
+  };
+}
 
-    const testObj = new ClickTracker();
-    const myToDo = new ToDoApp();
-    
-     window.onload=()=>{
-         myToDo.init();
-        };
-    
-    
+const tracker = new ClickTracker();
+const myToDo = new ToDoApp();
 
-    testObj.init();
+window.onload = () => {
+  myToDo.init();
+};
 
+tracker.init();
