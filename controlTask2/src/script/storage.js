87f4589f5dc01cee4d59709e96220a5df6b81583
konstantin <kostik1992@mail.ru>
2021-04-21 13:04:26 +0300

    маркировка прочитанных, сортировка

--- controlTask2/src/script/storage.js
+++ controlTask2/src/script/storage.js
@@ -8,30 +8,53 @@ export class Storage {
     getSavedBooks = () =>{
         return this.myBooksArray;
     }
+
+    getBooksId = () => {
+        const idArray = this.myBooksArray.map((item) => {
+            return item.id;
+        })
+        return idArray;
+    }
     
     loadBooks = () => {
-        if (localStorage.getItem(this.nameStorage)) {
-            this.myBooksArray = JSON.parse(localStorage.getItem(this.nameStorage));
-            return this.myBooksArray;
-        }
-        else return [];
-        
-    }
+      if (localStorage.getItem(this.nameStorage)) {
+        this.myBooksArray = JSON.parse(localStorage.getItem(this.nameStorage));
+
+        return this.myBooksArray;
+      } else return [];
+    };
 
     markAsRead = (id) => {
+      this.myBooksArray.forEach((item) => {
+        id === item.id ? (item.read = true) : "";
+      });
+      localStorage.setItem(this.nameStorage, JSON.stringify(this.myBooksArray));
+    };
 
+    showHelp = (mas) =>{
+        mas.forEach(element => {
+           console.log(element); 
+        });
     }
 
     removeBook = (id) => {
-        
-
-
+       
+        this.myBooksArray = this.myBooksArray.filter((item)=>{
+            return item.id !== id;
+        })
+      
         localStorage.setItem(this.nameStorage, JSON.stringify(this.myBooksArray))
 
     }
 
-    saveBooks = (book) => {
-        this.myBooksArray.push(book);
-        localStorage.setItem(this.nameStorage, JSON.stringify(this.myBooksArray))
+    addBooks = (book) => {
+    
+        if(!this.getBooksId().includes(book.id)){
+          
+            this.myBooksArray = [...this.myBooksArray, book];
+            localStorage.setItem(this.nameStorage, JSON.stringify(this.myBooksArray))
+        }
+        else { alert(`${book.title} уже добавлена в список`)}
+     
     }
 }
